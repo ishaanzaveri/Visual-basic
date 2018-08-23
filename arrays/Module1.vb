199a114961e24133712e730063f57627d13d1226
ishaanzaveri <39041074+ishaanzaveri@users.noreply.github.com>
2018-08-23 10:42:05 +0530

    changes to arrays

--- arrays/Module1.vb
+++ arrays/Module1.vb
@@ -2,21 +2,21 @@
 
     Sub Main()
         Dim n As Integer
+        Dim array(30) As Integer
         Console.WriteLine("how long is your array")
         n = Console.ReadLine()
-        Dim array(n) As Integer
         For i = 1 To n
-            Console.ReadLine()
+            array(i) = Console.ReadLine()
         Next
-        Console.WriteLine("max = " & Max(array) & " min = " & Min(array) & " avg = " & Avg(array))
+        Console.WriteLine("max = " & Max(array, n) & " min = " & Min(array, n) & " avg = " & Avg(array, n))
         Console.ReadLine()
     End Sub
 
 
-    Function Max(ByVal array) As Integer
+    Function Max(ByVal array, ByVal n) As Integer
         Dim maxN As Integer
         maxN = array(1)
-        For i = 2 To array.length() - 1
+        For i = 2 To n
             If array(i) > maxN Then
                 maxN = array(i)
             End If
@@ -24,21 +24,21 @@
         Return maxN
     End Function
 
-    Function Min(ByVal array) As Integer
+    Function Min(ByVal array, ByVal n) As Integer
         Dim minN As Integer
         minN = array(1)
-        For i = 2 To array.length - 1
+        For i = 2 To n
             If array(i) < minN Then
                 minN = array(i)
             End If
         Next
         Return minN
     End Function
-    Function Avg(ByVal array) As Integer
+    Function Avg(ByVal array, ByVal n) As Integer
         Dim sum As Integer = 0
-        For i = 1 To array.length - 1
+        For i = 1 To n
             sum = sum + array(i)
         Next
-        Return sum / array.length
+        Return sum / n
     End Function
 End Module
