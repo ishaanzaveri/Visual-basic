6b0f09fe7998ea051ef40eb248a87d256c5616e8
ishaanzaveri <39041074+ishaanzaveri@users.noreply.github.com>
2018-10-01 10:35:01 +0530

    initial commit

---
+++ fileHandling/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim fileHandle As IO.StreamWriter ' define it as an object of class IO
+        Dim str As String
+        fileHandle = New IO.StreamWriter("sample.txt") ' create object and file 
+        fileHandle.WriteLine("Hello World!") ' write to file 
+        fileHandle.Close() ' close file
+
+        Dim fileRead As IO.StreamReader
+        fileRead = New IO.StreamReader("sample.txt")
+        str = fileRead.ReadLine()
+        Console.WriteLine(str)
+        fileRead.Close()
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
