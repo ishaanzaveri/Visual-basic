6715296f7b4670db5c6875c0e10ebc71d4df22f9
ishaanzaveri <39041074+ishaanzaveri@users.noreply.github.com>
2018-12-22 17:40:49 +0530

    1.2.1

--- PreRealease/Module1.vb
+++ PreRealease/Module1.vb
@@ -1,10 +1,26 @@
 ﻿Module Module1
+    Sub outputting(ByVal arrNameEmail)
+        Dim hashno As Integer = 0
+        Console.WriteLine("Name                    Email") '10+5=25
+        For i = 1 To 3
+            If arrNameEmail(i) <> "" Then
+                For j = 1 To Len(arrNameEmail(i))
+                    If Mid(arrNameEmail(i), j, 1) = "#" Then
+                        hashno = j
+                    End If
+                Next
+                Console.Write(Left(arrNameEmail(i), hashno - 1) & Space(25 - hashno))
+                Console.Write(Right(arrNameEmail(i), Len(arrNameEmail(i)) - hashno))
+                Console.WriteLine()
+            End If
+        Next
+    End Sub
 
     Sub Main()
         Dim Name As String
         Dim Email As String
         Dim ArrNameEmail(3) As String
-        Dim hashno As Integer
+
 
         'Task 1.1
         For i = 1 To 3
@@ -15,19 +31,9 @@
             ArrNameEmail(i) = Name & "#" & Email
         Next
 
-        Console.WriteLine("Name                    Email") '10+5=25
-        For i = 1 To 3
-            If ArrNameEmail(i) <> "" Then
-                For j = 1 To Len(ArrNameEmail(i))
-                    If Mid(ArrNameEmail(i), j, 1) = "#" Then
-                        hashno = j
-                    End If
-                Next
-                Console.Write(Left(ArrNameEmail(i), hashno - 1) & Space(25 - hashno))
-                Console.Write(Right(ArrNameEmail(i), Len(ArrNameEmail(i)) - hashno))
-                Console.WriteLine()
-            End If
-        Next
+        outputting(ArrNameEmail)
+
+
         Console.ReadLine()
     End Sub
 
