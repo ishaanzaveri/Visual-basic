57640fc41c0a6b9480d4ed389c1f1c385beb52e2
ishaanzaveri <39041074+ishaanzaveri@users.noreply.github.com>
2019-01-03 10:07:38 +0530

    Task 2.2
    Added NameSearch for finding names

--- PrereleaseTask2/Module2.vb
+++ PrereleaseTask2/Module2.vb
@@ -23,5 +23,23 @@
         Next
         FileWriter.Close()
     End Sub
-
+    Sub NameSearch(ByRef FileReader)
+        FileReader = New System.IO.StreamReader("Log.txt")
+        Dim FileLine As String
+        Dim Name As String
+        Dim found As Boolean = False
+        Console.WriteLine("Enter the Name you want to find")
+        Name = Console.ReadLine()
+        For i = 1 To 3
+            FileLine = FileReader.ReadLine()
+            If Left(FileLine, Len(Name)) = Name Then
+                Console.WriteLine(Name & "'s email is " & Right(FileLine, Len(FileLine) - Len(Name) - 1))
+                found = True
+            End If
+        Next
+        If Not found Then
+            Console.WriteLine("Name not found")
+        End If
+        FileReader.Close()
+    End Sub
 End Module
