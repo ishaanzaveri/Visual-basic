c2cf67857902f98a2860c74d32126b8bbd4a3eb7
ishaanzaveri <39041074+ishaanzaveri@users.noreply.github.com>
2019-01-07 10:24:25 +0530

    task 2.5 and 2.6
    changed data structure to file lines per person

--- PrereleaseTask2/Module2.vb
+++ PrereleaseTask2/Module2.vb
@@ -12,32 +12,50 @@
     End Sub
     Sub Inputting(ByRef FileWriter As IO.StreamWriter) '2.1
         FileWriter = New System.IO.StreamWriter("Log.txt")
-        Dim Name As String
+        Dim StdId As String
         Dim Email As String
+        Dim HomeAdd As String
+        Dim Tutor As String
 
         'Task 1.1
         For i = 1 To 3
             Console.WriteLine("Enter Name")
-            Name = Console.ReadLine()
+            StdId = Console.ReadLine()
+            FileWriter.WriteLine(StdId)
             Console.WriteLine("Enter Email")
             Email = Console.ReadLine()
-            FileWriter.WriteLine(Name & "#" & Email)
+            FileWriter.WriteLine(Email)
+            Console.WriteLine("Enter Home Adress")
+            HomeAdd = Console.ReadLine()
+            FileWriter.WriteLine(HomeAdd)
+            Console.WriteLine("Enter tutor")
+            Tutor = Console.ReadLine()
+            FileWriter.WriteLine(Tutor)
+            'FileWriter.WriteLine(Name & "#" & Email)
+            FileWriter.WriteLine() ' 5 lines per person
         Next
         FileWriter.Close()
     End Sub
     Sub NameSearch(ByRef FileReader) '2.2
         FileReader = New System.IO.StreamReader("Log.txt")
         Dim FileLine As String
-        Dim Name As String
+        Dim StdId As String
         Dim found As Boolean = False
         Console.WriteLine("Enter the Name you want to find")
-        Name = Console.ReadLine()
+        StdID = Console.ReadLine()
         For i = 1 To 3
             FileLine = FileReader.ReadLine()
-            If Left(FileLine, Len(Name)) = Name Then
-                Console.WriteLine(Name & "'s email is " & Right(FileLine, Len(FileLine) - Len(Name) - 1))
+            If FileLine = StdId Then
+                FileLine = FileReader.ReadLine()
+                Console.WriteLine(FileLine)
+                'Console.WriteLine(Name & "'s email is " & Right(FileLine, Len(FileLine) - Len(Name) - 1))
                 found = True
+            Else
+                FileReader.ReadLine()
             End If
+            For j = 3 To 5
+                FileReader.ReadLine()
+            Next
         Next
         If Not found Then
             Console.WriteLine("Name not found")
@@ -63,26 +81,31 @@
                 End If
             Next
             If found Then
-                Console.WriteLine(Left(FileLine, HashNoFinder(FileLine) - 1))
+                Console.WriteLine(FileLine)
             ElseIf frequency < 1 Then
                 Console.WriteLine("Not found")
             End If
+            For j = 2 To 5
+                FileReader.ReadLine()
+            Next
         Next
         FileReader.Close()
     End Sub
-    Function HashNoFinder(ByRef str) As Integer
-        Dim hash As Integer
-        For j = 1 To Len(str)
-            If Mid(str, j, 1) = "#" Then
-                hash = j
-            End If
-        Next
-        Return hash
-    End Function
+    'Function HashNoFinder(ByRef str) As Integer
+    'Dim hash As Integer
+    'For j = 1 To Len(str)
+    'If Mid(str, j, 1) = "#" Then
+    '           hash = j
+    'End If
+    'Next
+    'Return hash
+    'End Function
     Sub Appened(ByRef FileWriter) ' 2.4
         FileWriter = New System.IO.StreamWriter("Log.txt", True)
         Dim Name As String
         Dim Email As String
+        Dim HomeAdd As String
+        Dim Tutor As String
         Dim append As Boolean = True
         Console.WriteLine("Do you want to append your File, if yes enter the appended data.After you are done appending enter a . as a rouge Values for name and email")
         Do While append
@@ -90,8 +113,15 @@
             Name = Console.ReadLine()
             Console.WriteLine("Enter Email")
             Email = Console.ReadLine()
-            If Name <> "." And Email <> "." Then
-                FileWriter.WriteLine(Name & "#" & Email)
+            Console.WriteLine("Enter Home address")
+            HomeAdd = Console.ReadLine()
+            Console.WriteLine("Enter tutor")
+            Tutor = Console.ReadLine()
+            If Name <> "." And Email <> "." And HomeAdd <> "." And Tutor <> "." Then
+                FileWriter.WriteLine(Name)
+                FileWriter.WriteLine(Email)
+                FileWriter.WriteLine(HomeAdd)
+                FileWriter.WriteLine(Tutor)
             Else
                 append = False
             End If
